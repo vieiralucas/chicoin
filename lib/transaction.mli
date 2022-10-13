851b197952ea77f0aaee725e3ddc6cf876cf7dd8
Lucas Vieira <vieiralucas4@gmail.com>
2022-10-13 09:28:34 -0300

    Transaction.Secret and Transaction.Key

--- lib/transaction.mli
+++ lib/transaction.mli
@@ -1,1 +1,19 @@
+module Secret : sig
+  type secret = Secp256k1.Key.secret Secp256k1.Key.t
+  type t = secret
+
+  val generate : secret
+  val to_b58 : secret -> string
+  val of_b58 : string -> secret option
+end
+
+module Key : sig
+  type key = Secp256k1.Key.public Secp256k1.Key.t
+  type t = key
+
+  val of_secret : Secret.t -> key
+end
+
+val mint_secret : Secret.t
+
 type transaction = unit
